e9a273243c80bc7fefe4260cec3fbd2778bd4724
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-02 10:57:14 +0530

    power

--- 1.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-res = 0
-for i in range(1, n+1):
-    res += pow(2, i)
-print(res)
--- pass_combin.cpp
+++ pass_combin.cpp
@@ -4,10 +4,6 @@ using namespace std;
 int main(){
     long long n,res = 0,power_of_two=1;
     cin>>n;
-    // for(long long i = 1;i<=n;i++) res += pow(2,i);
-    for (long long i = 1; i <= n; i++) {
-        res += power_of_two;  // Add current power of 2 to the result
-        power_of_two *= 2;    // Calculate next power of 2
-    }
+    for(long long i = 1;i<=n;i++) res += pow(2,i);
     cout<<res;
 }
