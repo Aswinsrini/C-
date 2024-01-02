f52a9622478c2da05c97a698272b1057dd8cab40
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-02 11:32:11 +0530

    Bloody Sweet..

--- bloody_sweet.cpp
+++ bloody_sweet.cpp
@@ -1,33 +1,29 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    long long n,val,count = 0; 
+    int n,val; 
     cin>>n;
     vector<int> arr;
-    for(int i =0;i<n;i++) 
-    {
+    for(int i =0;i<n;i++) {
         cin >> val;
         arr.push_back(val);
-        if(val == 0) count++;
     }
-    long long res = 1,count_zero = 0;
-    if(count == n){
+    if(count(arr.begin(),arr.end(),1) == 0){
         cout<<0;
     }
     else{
-
-    bool set = false;
-    for(int ele : arr){
-        if(ele == 1){
-            set = true;
-            res *= (count_zero+1);
-            count_zero = 0;  
+        long long res = 1,count_zero = 0;
+        bool set = false;
+        for(int ele : arr){
+            if(ele == 1){
+                set = true;
+                res *= (count_zero+1);
+                count_zero = 0;  
+            }
+            else if(set){
+                count_zero++;
+            }
         }
-        else if(set){
-            count_zero++;
-        }
-    }
-    cout<<res;
+        cout<<res;
     }
 }
