3a60a3f41ad5c78eb22ff56d66048262151df171
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-02 15:49:58 +0530

    Talentio

---
+++ talentio/ball_game.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,val,res=0;
+    cin>> n;
+    for(int i= 0;i<n;i++){
+        cin >> val;
+        res ^= val;
+    }
+    if(res == 0) cout<<"B wins";
+    else cout<<"A wins";
+    
+}
