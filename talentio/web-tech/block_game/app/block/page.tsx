c3a3fef58541def6b964bd2629c8aac6af6fd285
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-05 15:58:03 +0530

    block game

--- talentio/web-tech/block_game/app/block/page.tsx
+++ talentio/web-tech/block_game/app/block/page.tsx
@@ -1,27 +1,39 @@
 "use client";
 import { useState } from "react";
 const Page = () => {
-  const [Position, setPosition] = useState({ top: 100, left: 10 });
+  const [Position, setPosition] = useState({ top: 10, left: 10 });
   const [Score, setScore] = useState(0);
   const changePosition = () => {
     const pos = {
-      top: Math.floor(Math.random() * window.innerHeight),
-      left: Math.floor(Math.random() * window.innerHeight),
+      top: Math.floor(Math.random() * 90),
+      left: Math.floor(Math.random() * 90),
     };
     setScore((prevState) => prevState + 1);
-    // console.log(Position);
     setPosition(pos);
   };
+
+  const reStartGame = () => {
+    setScore(0);
+  };
   return (
-    <div className="h-screen w-max">
-      <h1 className="">Score :{Score}</h1>
+    <div className="min-h-screen bg-slate-600">
+      <center>
+        <h1 className="font-bold text-green-400 text-3xl">Score : {Score}</h1>
+        <button
+          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={reStartGame}
+        >
+          Restart the Game!
+        </button>
+      </center>
+
       <div
         className="w-20 h-20 bg-red-700 absolute"
         id="block"
         onClick={changePosition}
         style={{
-          top: `${Position.top}px`,
-          left: `${Position.left}px`,
+          top: `${Position.top}%`,
+          left: `${Position.left}%`,
         }}
       ></div>
     </div>
