90166acae9672a947ac89fd72361e76826baa7b8
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-14 23:40:46 +0530

    Auth...

--- talentio/web-tech/block_game/app/page.tsx
+++ talentio/web-tech/block_game/app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>happpiee</main>;
+  const { status, data: session } = useSession();
+  // if (status === "loading") return null;
+
+  return (
+    <main>
+      <div className="m-10">
+        {status === "loading" && <div>Loading User Details...</div>}
+        {status === "authenticated" && (
+          <div>
+            <pre>
+              Name : {session.user!.name}
+              <br></br>
+              Email-id : {session.user!.email}
+            </pre>
+            {/* <Image
+              src={session.user!.image as string}
+              height={50}
+              width={50}
+              alt="User-Image"
+            /> */}
+          </div>
+        )}
+        {status === "unauthenticated" && (
+          <Link
+            href="/api/auth/signin"
+            className="border border-solid bg-blue-400 p-5 border-r-2"
+          >
+            Sign with Google
+          </Link>
+        )}
+      </div>
+    </main>
+  );
 }
