9bc2a92ee779c01348537dbab6b5aa2867c8b1f8
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-14 13:04:00 +0530

    Prisma with products model

--- talentio/web-tech/block_game/app/api/products/route.tsx
+++ talentio/web-tech/block_game/app/api/products/route.tsx
@@ -2,11 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import schema from "./schema";
 
-export async function GET() {
-  const users = await prisma.user.findMany();
-  return NextResponse.json(users);
-}
-
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
