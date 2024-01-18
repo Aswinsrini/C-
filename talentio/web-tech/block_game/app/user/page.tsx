0b10437013d54c746178a1f656fc4c2232e27a84
Aswin <aswinsrinivasan2004@gmail.com>
2024-01-18 19:41:11 +0530

    Next/Image

--- talentio/web-tech/block_game/app/user/page.tsx
+++ talentio/web-tech/block_game/app/user/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import Image from "next/image";
+import leetcode from "@/public/images/leetcode.png";
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div className="relative h-screen">
+      <Image
+        src={leetcode}
+        alt="LeetCode"
+        className="object-cover"
+        fill
+        // quality={100}
+        sizes="(max-width: 768px) 100vw, 33vw"
+        unoptimized={true}
+      />
+    </div>
+  );
 };
 
 export default User;
