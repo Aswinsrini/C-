3575b1a78813218a3a74a8f3aad7c3d6854a3623
Aswinsrini <aswinsrinivasan2004@gmail.com>
2024-05-25 17:56:25 +0530

    25-May

---
+++ geeksforgeeks/25.05.2024.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        long long res = 0,temp = 0;
+        for(int i = 0;i<n;i++){
+            if(arr[i] <= k){
+                temp += arr[i];
+            }
+            else{
+                res = max(res,temp);
+                temp = 0;
+            }
+        }
+        res = max(res,temp);
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
