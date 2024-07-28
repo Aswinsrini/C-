1f701ffcdb4b29e38e4b4d8b7b7b78659c97bb3e
aswinsrini <aswinsrinivasan2004@gmail.com>
2024-07-28 10:00:43 +0530

    28-Jul

---
+++ geeksforgeeks/07-july/28.07.2024.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        string res;
+        unordered_set<char>mp;
+        for(char ch : str){
+            if(mp.find(ch) == mp.end()){
+                res += ch;
+                mp.insert(ch);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
